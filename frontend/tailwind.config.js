adb7cda183d5dc8e07dfaecd5addbf0ec43703c7
Andrew Kari <872347+xparnie@users.noreply.github.com>
2023-04-07 17:33:50 -0500

    removes flowbite from tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,6 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
    content: [
-      "./node_modules/flowbite-react/**/*.js",
       "./app/**/*.{js,ts,jsx,tsx}",
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
@@ -19,5 +18,4 @@ module.exports = {
          },
       },
    },
-   plugins: [require("flowbite/plugin")],
 };
