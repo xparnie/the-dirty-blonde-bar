fe0d91bd776e62a46aecbd5752314c16de4a9e8d
Andrew Kari <872347+xparnie@users.noreply.github.com>
2023-04-10 19:37:23 -0500

    updates hours

--- frontend/components/Hours.tsx
+++ frontend/components/Hours.tsx
@@ -5,35 +5,38 @@ const Hours: React.FC = (): JSX.Element => (
       <div className="flex flex-col w-full lg:w-3/4">
          <div className="mb-5">
             <h2 className="text-4xl md:text-5xl text-white font-bold">Hours</h2>
+            <span className="inline-block mt-1 text-md md:text-lg text-white font-bold">
+               (Kitchen closes at 8 PM)
+            </span>
          </div>
          <ul className="flex flex-col space-y-1 w-full text-white text-xl">
-            <li className="flex justify-between">
-               <span>Sunday</span>
-               <span>8 AM - 8 PM</span>
-            </li>
             <li className="flex justify-between">
                <span>Monday</span>
                <span>Closed</span>
             </li>
             <li className="flex justify-between">
                <span>Tuesday</span>
-               <span>8 AM - 8 PM</span>
+               <span>11 AM - 1 AM</span>
             </li>
             <li className="flex justify-between">
                <span>Wednesday</span>
-               <span>8 AM - 8 PM</span>
+               <span>11 AM - 1 AM</span>
             </li>
             <li className="flex justify-between">
                <span>Thursday</span>
-               <span>8 AM - 8 PM</span>
+               <span>11 AM - 1 AM</span>
             </li>
             <li className="flex justify-between">
                <span>Friday</span>
-               <span>8 AM - 8 PM</span>
+               <span>11 AM - 1 AM</span>
             </li>
             <li className="flex justify-between">
                <span>Saturday</span>
-               <span>8 AM - 8 PM</span>
+               <span>8 AM - 1 AM</span>
+            </li>
+            <li className="flex justify-between">
+               <span>Sunday</span>
+               <span>8 AM - 11 PM</span>
             </li>
          </ul>
       </div>
